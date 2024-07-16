74f871871a539ff6466bce1ba160cdb82ee33625
widya <widyadhana92@gmail.com>
2024-07-16 21:52:47 +0700

    fix: change navbar background

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -9,7 +9,6 @@ import { ShopContext } from "../../context/ShopContext";
 export default function navbar() {
   const [isClicked, setIsClicked] = useState(false);
   const [scrolledNavbar, setScrolledNavbar] = useState(false);
-  const [menu, setMenu] = useState("home");
   const { getTotalItems } = useContext(ShopContext);
 
   function toggleNavbar() {
@@ -30,7 +29,7 @@ export default function navbar() {
     <header
       className={`${scrolledNavbar ? "shadow-md" : "shadow-none"} ${
         isClicked ? "shadow-none" : "shadow-md"
-      } px-8 md:px-16 py-4 sticky top-0 z-20 bg-white bg-opacity-10 backdrop-blur-sm`}
+      } px-8 md:px-16 py-4 sticky top-0 z-20 bg-white bg-opacity-100 md:bg-opacity-10 backdrop-blur-sm`}
     >
       <nav className="w-full flex justify-between items-center">
         <div>
@@ -41,7 +40,7 @@ export default function navbar() {
         <ul
           className={`${
             isClicked ? "top-16" : "-top-64"
-          } absolute flex flex-col md:flex-row md:static gap-4 md:gap-16 left-0 w-full md:w-auto py-4 md:py-0 bg-white md:bg-opacity-0 bg-opacity-10 backdrop-blur-sm md:backdrop-blur-none`}
+          } absolute flex flex-col md:flex-row md:static gap-4 md:gap-16 left-0 w-full md:w-auto py-4 md:py-0 bg-white md:bg-opacity-0 bg-opacity-100`}
         >
           <li>
             <NavLink
@@ -88,8 +87,8 @@ export default function navbar() {
         </ul>
         <div
           className={`${
-            isClicked ? "top-[200px]" : "-top-64"
-          } w-full md:w-auto flex gap-6 md:static absolute left-0 px-8 md:px-0 py-4 md:py-0 bg-white bg-opacity-10 md:bg-opacity-0 backdrop-blur-sm md:backdrop-blur-none shadow-md  md:shadow-none`}
+            isClicked ? "top-[199px]" : "-top-64"
+          } w-full md:w-auto flex gap-6 md:static absolute left-0 px-8 md:px-0 py-4 md:py-0 bg-white bg-opacity-100 md:bg-opacity-0 backdrop-blur-sm md:backdrop-blur-none shadow-md  md:shadow-none`}
         >
           <Link to="/Cart" onClick={() => setMenu("cart")}>
             <FiShoppingCart className="h-6 w-6 cursor-pointer" />
