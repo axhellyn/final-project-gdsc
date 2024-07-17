8a204024170bd879b21aa1ac9a53024522b442ba
widya <widyadhana92@gmail.com>
2024-07-17 11:01:30 +0700

    feat: add firebase configuration

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMSCMEAoWjnZYE1Q5DXLwFcwdD_ZowgeM",
+  authDomain: "donut-shop-4b9b0.firebaseapp.com",
+  projectId: "donut-shop-4b9b0",
+  storageBucket: "donut-shop-4b9b0.appspot.com",
+  messagingSenderId: "987428736363",
+  appId: "1:987428736363:web:fb77c086060f9cb93fd02e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
